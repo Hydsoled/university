84af136537b34de92da701c2ea84ff0ef1a25047
hydsoled <hydsoled@gmail.com>
2020-09-26 21:10:59 +0400

    add server

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const { FacebookAdapter } = require('botbuilder-adapter-facebook')
 const app = express()
+
 require('dotenv').config()
 const PORT = process.env.PORT
+const adapter = new FacebookAdapter({
+    verify_token: process.env.FACEBOOK_VERIFY_TOKEN,
+    app_secret: process.env.FACEBOOK_APP_SECRET,
+    access_token: process.env.FACEBOOK_ACCESS_TOKEN
+})
 
-app.get('/', (req, res) => {
-    res.send('gamarjoba')
+app.post('/api/messages', (req, res) => {
+
 })
 
 app.listen(PORT, ()=>{
